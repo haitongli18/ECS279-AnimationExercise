5be50bfc551bb96545b25731858e3abf6154d191
Haitong Li <haitongli@Haitongs-MacBook-Pro.local>
2020-03-11 22:02:28 -0700

    Animation exercise Unity project

---
+++ Animation Exercise/Assets/switchScript1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class switchScript1 : MonoBehaviour
+{
+   public KeyCode MyKey;
+     public string MyTrigger;
+ 
+     void Update()
+     {
+         if (Input.GetKey(MyKey))
+         {
+             GetComponent<Animator>().SetTrigger(MyTrigger);
+         }
+     }
+}
